29cec64df298bbd52ed0f28c29ff2efed2718ce6
Mindaugas Petronis <63074962+dzastsed@users.noreply.github.com>
2024-12-02 09:04:40 +0200

    update

--- LD10/lighttoggle.js
+++ LD10/lighttoggle.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const themeToggleButton = document.getElementById("theme-toggle");
     const currentTheme = localStorage.getItem("theme") || "light";
 
-    // Inicializuojame temą
     document.documentElement.setAttribute("data-theme", currentTheme);
 
-    // Perjungimo funkcija
     themeToggleButton.addEventListener("click", () => {
         const newTheme = document.documentElement.getAttribute("data-theme") === "light" ? "dark" : "light";
         document.documentElement.setAttribute("data-theme", newTheme);
